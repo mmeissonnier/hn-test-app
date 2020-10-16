a06e6019a744953f8b8553e45f464896f7345084
Mathieu Meissonnier <mmeissonnier@gmail.com>
2020-10-16 19:24:43 +0200

    chore: CircleCI setup

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,16 @@
-import styled from 'styled-components'
+import styled from 'styled-components';
 
 const Title = styled.h1`
   color: red;
   font-size: 50px;
-`
+`;
 
 export default function Home() {
-  return <Title>My page</Title>
+  return <Title>My page</Title>;
+}
+
+export async function getServerSideProps() {
+  return {
+    props: {}, // will be passed to the page component as props
+  };
 }
